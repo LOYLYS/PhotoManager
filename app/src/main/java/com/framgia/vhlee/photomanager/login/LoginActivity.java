accf9f0c16e6487df2c632db4afc08855216d7f6
Vo Huu Ly <vhlee1996@gmail.com>
2018-10-13 12:06:58 +0700

    Init project

---
+++ app/src/main/java/com/framgia/vhlee/photomanager/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.framgia.vhlee.photomanager.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.framgia.vhlee.photomanager.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
